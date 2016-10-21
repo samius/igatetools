9b0bd378b0a5be29a062e8d9620985c33b7a416b
milan <samius05@gmail.com>
2016-10-21 16:41:18 +0200

    prevadeni mime na priponu

--- src/Igate/Mime.php
+++ src/Igate/Mime.php
@@ -9,6 +9,17 @@ namespace Igate;
 class Mime
 {
     const
+        EXT_PDF = 'pdf',
+        EXT_TXT = 'pdf',
+        EXT_XML = 'pdf',
+        EXT_GIF = 'pdf',
+        EXT_JPEG = 'jpeg',
+        EXT_JPG = 'jpg',
+        EXT_PNG = 'png',
+        EXT_CSV = 'csv',
+        EXT_SWF = 'swf',
+        EXT_FLASH = 'flash';
+    const
         PDF      = 'application/pdf',
         TXT      = 'text/plain',
         XML      = 'text/xml',
@@ -30,33 +41,61 @@ class Mime
     public static function fromExtension($ext)
     {
         switch (strtolower($ext)) {
-            case 'pdf':
+            case self::EXT_PDF:
                 return self::PDF;
 
-            case 'txt':
+            case self::EXT_TXT:
                 return self::TXT;
 
-            case 'xml':
+            case self::EXT_XML:
                 return self::XML;
 
-            case 'gif':
+            case self::EXT_GIF:
                 return self::GIF;
 
-            case 'jpg':
-            case 'jpeg':
+            case self::EXT_JPEG:
+            case self::EXT_JPG:
                 return self::JPG;
 
-            case 'png':
+            case self::EXT_PNG:
                 return self::PNG;
 
-            case 'csv':
+            case self::EXT_CSV:
                 return self::CSV;
 
-            case 'swf':
-            case 'flash':
+            case self::EXT_FLASH:
+            case self::EXT_SWF:
                 return self::FLASH;
         }
 
         return self::DOWNLOAD;
+    }
+
+    /**
+     * @param $mime
+     * @return string
+     */
+    public static function getExtension($mime)
+    {
+        switch (strtolower($mime)) {
+            case self::PNG:
+                return self::EXT_PNG;
+            case self::JPG:
+                return self::EXT_JPG;
+            case self::PDF:
+                return self::EXT_PDF;
+            case self::TXT:
+                return self::EXT_TXT;
+            case self::XML:
+                return self::EXT_XML;
+            case self::GIF:
+                return self::EXT_GIF;
+            case self::CSV:
+                return self::EXT_CSV;
+            case self::FLASH:
+                return self::EXT_SWF;
+            default:
+                return '';
+        }
     }
 }
